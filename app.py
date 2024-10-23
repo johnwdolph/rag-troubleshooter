1d340928d096be6ae1e36eacbaa38ccb0bae85b3
John <29528014+johnwdolph@users.noreply.github.com>
2024-10-23 23:34:54 +0000

    Remove depricated chatbot data type passing

--- app.py
+++ app.py
@@ -12,19 +12,24 @@ from llama_index.embeddings.nvidia import NVIDIAEmbedding
 from llama_index.llms.nvidia import NVIDIA
 from llama_index.postprocessor.nvidia_rerank import NVIDIARerank
 
-import nemo_curator_utils as ncu
-
 load_dotenv()
 
-stream=True
+send_system_messages = True
+
+CHATBOT_LLM = 'meta/llama-3.1-70b-instruct'
+CHATBOT_LLM2 = 'meta/llama-3.1-8b-instruct'
+VISION_LLM = 'meta/llama-3.2-11b-vision-instruct'
+VISION_LLM_2 = 'microsoft/phi-3.5-vision-instruct'
 
 #configure application
 Settings.text_spllitter = SentenceSplitter(chunk_size=500)
 Settings.embed_model = NVIDIAEmbedding(mode="NV-Embed-QA", truncate="END")
-Settings.llm = NVIDIA(model="meta/llama-3.1-70b-instruct")
+Settings.llm = NVIDIA(model=CHATBOT_LLM2)
 
 # Check for API key (should be in .env)
 NVIDIA_API_KEY = ""
+LLAMA_VISION_API_KEY = os.getenv("LLAMA_VISION_API_KEY")
+
 if os.getenv('NVIDIA_API_KEY') is None:
     raise ValueError("No NVIDIA_API_KEY set...")
 else:
@@ -37,26 +42,37 @@ query_engine = None
 vector_store = MilvusVectorStore(uri='./milvus_store_context.db', dim=1024, overwrite=True)
 storage_context = StorageContext.from_defaults(vector_store=vector_store)
 
-def llama_vision_processor(image):
-    global nvidia_api_key
+# system messages
+system_messages = ["You are an assistant built to help diagnose issues with vehicles.",
+                   "When the user asks for help, try to reference the user-provided car type, codes, and description in context with your response.",
+                   "Make sure to verify if the car type and codes provided exist before discussing them. If necessary, correct these for the user and remeber your corrections.",
+                   "Try not to give generic advice unless the information is vague. Use the specific car type and p codes to figure out your solution."
+                   ]
 
-    invoke_url = 'https://ai.api.nvidia.com/v1/gr/meta/llama-3.2-90b-vision-instruct/chat/completions'
-    stream = True
+def llama_vision_processor(image):
+
+    invoke_url = "https://ai.api.nvidia.com/v1/gr/meta/llama-3.2-11b-vision-instruct/chat/completions"
+    stream = False
 
     with open(image, "rb") as f:
         image_b64 = base64.b64encode(f.read()).decode()
 
     assert len(image_b64) < 180_000, \
     "To upload larger images, use the assets API (see docs)"
+    
 
     headers = {
-    f"Authorization": "Bearer {nvidia_api_key}",
+    "Authorization": f"Bearer {NVIDIA_API_KEY}",
     "Accept": "text/event-stream" if stream else "application/json"
     }
 
     payload = {
-    "model": 'meta/llama-3.2-90b-vision-instruct',
+    "model": 'meta/llama-3.2-11b-vision-instruct',
     "messages": [
+        {
+            "role": "system",
+            "content": "Only provide information relevant to cars that may be useful in diagnosing issues"
+        },
         {
         "role": "user",
         "content": f'What is in this image? <img src="data:image/png;base64,{image_b64}" />'
@@ -69,30 +85,34 @@ def llama_vision_processor(image):
     }
 
     response = requests.post(invoke_url, headers=headers, json=payload)
-    print(response)
 
-    result = ""
-    if stream:
-        for line in response.iter_lines():
-            if line:
-                result += line.decode("utf-8")
-
-    return f"Image Analysis: {result}"
+    # Extract the content from the response
+    if response.status_code == 200:
+        response_json = response.json()
+        completion_text = response_json['choices'][0]['message']['content']
+        return completion_text
+    else:
+        print(f"Error: {response.status_code}")
+    
+    return None
 
 def process_issue_context(files, issue_description="", car_type="", OBD_codes=""):
     global index, query_engine, vector_store, storage_context
     try:
         documents = []
+        file_paths = []
         if files:
-            file_paths = [file.name for file in files]
+            
+            if files:
+                file_paths = [file.name for file in files]
             
             for file_path in file_paths:
                 
                 mime_type, _ = mimetypes.guess_type(file_path)
                 if mime_type and mime_type.startswith('image/'):
-                    print("image found")
                     image_analysis_result = llama_vision_processor(file_path)
-                    documents.append(Document(text=image_analysis_result, doc_id=file_path))
+                    if image_analysis_result:
+                        documents.append(Document(text=image_analysis_result, doc_id=file_path))
                 else:
                     directory = os.path.dirname(file_path)
                     reader = SimpleDirectoryReader(input_files=[file_path])
@@ -110,28 +130,6 @@ def process_issue_context(files, issue_description="", car_type="", OBD_codes=""
     
     except Exception as e:
         return f"Error loading info: {str(e)} - "
-
-def stream_response(message, history):
-    global query_engine
-    if query_engine is None:
-        yield history + [("Please provide information first", None)]
-        return
-    try:
-        response = query_engine.query(message)
-        partial_repsonse = ""
-        for text in response.response_gen:
-            partial_repsonse += text
-            yield history + [(message, partial_repsonse)]
-    except Exception as e:
-        yield history + [(message, f"Error processing query: {str(e)}")]
-
-def get_diagnostic(chatbot):
-    message = "What is the issue given my provided information?"
-    stream_response(message, chatbot)
-    return chatbot
-
-system_message = "You are an assistant built to help diagnose issues with vehicles."
-initial_messages = [gr.ChatMessage(role="system", content=system_message)]
 
 # create gradio interface
 with gr.Blocks() as app:
@@ -151,22 +149,45 @@ with gr.Blocks() as app:
         issue_description = gr.Textbox(label="Issue Description", placeholder="Describe the problem you're experiencing with the car", lines=5)
 
     #Buttons
-    generate_images = gr.Checkbox("Generate output images")
+    generate_images = gr.Checkbox(label="Generate output images")
     submit_button = gr.Button("Submit")
-    result_button = gr.Button("Get Diagnostic")
-
     user_params_output = gr.Textbox(label="User Input Status")
 
-    chatbot = gr.Chatbot(value=[[None, "Hi, ask me anything!"]])
+    chatbot = gr.Chatbot(value=[gr.ChatMessage(role='assistant', content='Hi, ask me anything!')], type='messages')
+
+    def user(user_message, history: list):
+        return history + [{'role': 'user', 'content': user_message}]
+
+    def bot(user_message, history: list):
+        global query_engine, system_messages, send_system_messages
+
+        if query_engine is None:
+            history.append({'role': 'assistant', 'content': "Hi, please provide some information first."})
+            yield history
+            return
+
+        if send_system_messages:
+            for msg in system_messages:
+                history.append({'role': 'system', 'content': msg})
+            send_system_messages = False  # Disable further system messages
+
+        bot_message = query_engine.query(user_message)
+
+        history.append({'role': 'assistant', 'content': ""})
+        for character in bot_message.response_gen:
+            history[-1]['content'] += character
+            time.sleep(0.05)
+            yield history
+
     msg = gr.Textbox(label="Enter your question", interactive=True)
 
-    submit_button.click(process_issue_context, inputs=[file_input, chatbot, issue_description, car_type, OBD_codes], outputs=user_params_output)
-    result_button.click(get_diagnostic, inputs=[file_input], outputs=[chatbot])
+    auto_query = gr.State("What is wrong and how can I solve it? Please give me a concise step-by-step solution if possible. Add proper line breaks between steps for readability.")
+    submit_button.click(process_issue_context, inputs=[file_input, issue_description, car_type, OBD_codes], outputs=user_params_output, queue=False).then(bot, inputs=[auto_query, chatbot], outputs=[chatbot])
 
     clear_btn = gr.Button("Clear")
 
     #events
-    msg.submit(stream_response, inputs=[msg, chatbot], outputs=[chatbot])
+    msg.submit(user, inputs=[msg, chatbot], outputs=[chatbot], queue=False).then(bot, inputs=[msg, chatbot], outputs=[chatbot])
     msg.submit(lambda: "", outputs=[msg])
     clear_btn.click(lambda: None, None, chatbot, queue=False)
 
